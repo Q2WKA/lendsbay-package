7535c0c64f67493aa188a3a800eccdd5edcfeb60
Renat Kutushev <93474253+Q2WKA@users.noreply.github.com>
2022-04-14 10:48:06 +0300

    Added first two functions

---
+++ functions/time_func.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def add_month(date: pd.Timestamp):
+    if date.month < 12:
+        d = str(date.year)+'-'+str(date.month+1)+'-'+str(date.day)+' '+str(date.time)
+        return pd.to_datetime(d)
+    else:
+        d = str(date.year+1)+'-'+str(1)+'-'+str(date.day)+' '+str(date.time)
+        return pd.to_datetime(d)
+
+def drop_tz(table: pd.DataFrame):  
+    if 'datetime64[ns, UTC]' in table.dtypes.value_counts(0):  
+        for i in table.select_dtypes(include=['datetime64[ns, UTC]'], exclude=None).columns:  
+            table.loc[:,i] = table.loc[:,i].dt.tz_localize(None)  
+          
+    return(table)
